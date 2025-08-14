8b5f0d408187703ba215cd76802331f6eec15761
Khizra-shahzad <khizrashahzad22@gmail.com>
2025-08-14 17:20:23 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH ||'/Fire-Base-Contact-App-Project'
+  base:'/Fire-Base-Contact-App-Project',
 });
